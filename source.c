0d4442d500a523655d6c95ca8a8e24c458cba34d
kreininmv <70326773+kreininmv@users.noreply.github.com>
2020-08-28 01:19:28 +0300

    Add files via upload

--- source.c
+++ source.c
@@ -2,55 +2,117 @@
 #include <math.h>
 
 #define per_error 0.0000000005
+#define any_sol 0x1E
+
+/* Check inaccuracy of numbers.
+ * ARGUMENTS:
+ *   - first number:
+ *       double a;
+ *   - second number:
+ *       double b;
+ * RETURNS:
+ *   (int) true or false.
+ */
 int Compr(double a, double b)
 {
   if (fabs(a - b) <= per_error)
     return 1;
   return 0;
-}
+} /* End of 'Compr' function */
+
+/* Solve linear equation function.
+ * ARGUMENTS:
+ *   - coefficient b:
+ *       double b;
+ *   - coefficient c:
+ *       double c;
+ *   - pointer to solution 1:
+ *       double *sol1;
+ *   - pointer to solution 2:
+ *       double *sol2;
+ * RETURNS:
+ *   (int) number of desicions.
+ */
 int SolveLin(double b, double c, double *sol1, double *sol2)
 {
   if (b == 0 && c == 0)
-    return 0x1E;
+    return any_sol;
   if (b == 0)
     return 0;
   *sol1 = *sol2 = -c / b;
   return 1;
-}
+} /* End of 'SolveLin' function */
+
+/* Solve quadratic ecuation function.
+ * ARGUMENTS:
+ *   - coefficient a:
+ *       double a;
+ *   - coefficient b:
+ *       double b;
+ *   - coefficient c:
+ *       double c;
+ *   - pointer to solution 1:
+ *       double *sol1;
+ *   - pointer to solution 2:
+ *       double *sol2;
+ * RETURNS:
+ *   (int) number of desicions.
+ */
 int SolveQuad(double a, double b, double c, double *sol1, double *sol2)
 {
-  double Dis = sqrt(b * b - 4 * a * c);
-  *sol1 = (-b + Dis) / (2 * a);
-  *sol2 = (-b - Dis) / (2 * a);
+  double Dis = b * b - 4 * a * c;
+  if (Dis < 0)
+    return 0;
+  *sol1 = (-b + sqrt(Dis)) / (2 * a);
+  *sol2 = (-b - sqrt(Dis)) / (2 * a);
   return (Compr(Dis, 0)) ? 1 : 2;
-}
+} /* Enf of 'SolveQuad' function */
 
+/* Solve equation function.
+ * ARGUMENTS: 
+ *   - coefficient a:
+ *       double a;
+ *   - coefficient b:
+ *       double b;
+ *   - coefficient c:
+ *       double c;
+ *   - pointer to solution 1:
+ *       double *sol1;
+ *   - pointer to solution 2:
+ *       double *sol2;
+ * RETURNS: 
+ *   (int) number of desicions.
+ */
 int SolveEq(double a, double b, double c, double *sol1, double *sol2)
 {
   if (a == 0)
     return SolveLin(b, c, sol1, sol2);
-  if (b * b - 4 * a * c >= 0)
-    return SolveQuad(a, b, c, sol1, sol2);
+  return SolveQuad(a, b, c, sol1, sol2);
+} /* End of 'SolveEq' function */
 
-  /* Discriminant < 0, and equations has no solutions */
-  return 0;
-}
+
+/* Main function.
+ * ARGUMENTS: None.
+ * RETURNS: None.
+ */
 void main(void)
 {
   double a = 0xEF, b = 0xEF, c = 0xEF, sol1 = 0xEF, sol2 = 0xEF;
   int res = 0xEF;
- 
-  scanf("%lg %lg %lg", &a, &b, &c);
-  if (a == 0xEF || b == 0xEF || c == 0xEF)
+  
+  printf("Progrram of solving equation, entered coefficients:\n");
+  if (scanf("%lg %lg %lg", &a, &b, &c) != 3)
   {
-    printf("You entered the wrong data, write numbers\n");
+    printf("Something going wrong, please enter numbers!\n");
     return;
   }
+  printf("\n");
+
   res = SolveEq(a, b, c, &sol1, &sol2);
   
   switch (res)
   {
-  case 0x1E:
+  case any_sol:
     printf("Any number is solution!\n");
     break;
   case 0:
@@ -66,4 +128,4 @@ void main(void)
   getchar();
   getchar();
   
-}
+} /* End of 'main' function */
